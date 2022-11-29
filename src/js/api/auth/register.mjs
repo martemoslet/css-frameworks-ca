dfe50222fabf25216d22a6936bd12f479d458d2e
martemoslet <martemoslet@gmail.com>
2022-11-29 22:21:11 +0100

    Adds auth code

--- src/js/api/auth/register.mjs
+++ src/js/api/auth/register.mjs
@@ -4,17 +4,18 @@ const action = "/auth/register";
 const method = "post";
 
 export async function register(profile) {
-    const registerURL = API_SOCIAL_URL + action;
-    const body = JSON.stringify(profile);
+  const registerURL = API_SOCIAL_URL + action;
+  const body = JSON.stringify(profile);
 
-    const response = await fetch(registerURL, {
-        headers: {
-            "Content-Type": "application/json"
-        },
-        method,
-        body
-    })
+  const response = await fetch(registerURL, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method,
+    body,
+  });
 
-    const result = await response.json()
-    console.log(result)
+  const result = await response.json();
+  alert("Registration successful");
+  return result;
 }
