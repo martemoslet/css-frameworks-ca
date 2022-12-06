a0185354b161c6ca5bf8b6cd398676cbb555a1e5
martemoslet <martemoslet@gmail.com>
2022-12-06 16:40:58 +0100

    Updates js

--- src/js/api/authFetch.mjs
+++ src/js/api/authFetch.mjs
@@ -9,7 +9,7 @@ export function headers() {
     }
 }
 
-export async function authFetch(url, options) {
+export async function authFetch(url, options = {}) {
     return fetch(url, {
         ...options,
         headers: headers()
