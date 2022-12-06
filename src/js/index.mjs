9ce6e48d2975979c00a3ae243e7933909d7195c9
martemoslet <martemoslet@gmail.com>
2022-12-06 16:40:47 +0100

    Updates listeners

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,10 +1,30 @@
-import { setRegisterFormListener } from "./handlers/register.mjs";
-import { setLoginFormListener } from "./handlers/login.mjs";
+import * as listeners from "./handlers/index.mjs"
+import * as templates from "./templates/index.mjs"
+import * as postMethods from "./api/posts/index.mjs"
 
 const path = location.pathname;
 
 if (path === '/profile/login/') {
-    setLoginFormListener()
+    listeners.setLoginFormListener()
 } else if (path === '/profile/register/') {
-    setRegisterFormListener()
+    listeners.setRegisterFormListener()
+} else if (path === '/post/create/') {
+    listeners.setCreatePostFormListener()
+} else if (path === '/post/edit/') {
+    listeners.setUpdatePostListener()
 }
+
+
+// async function testTemplate() {
+   // const posts = await postMethods.getPosts();
+    // const container = document.querySelector("#post");
+    // templates.renderPostTemplates(posts, container);
+// }
+
+// testTemplate()
+
+// post.createPost()
+// post.updatePost()
+// post.removePost()
+// post.getPost()
+// postMethods.getPosts().then(console.log)
