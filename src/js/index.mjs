6b022d96cb933b5b4682624e1523734ae8988d0b
martemoslet <martemoslet@gmail.com>
2022-12-07 20:26:45 +0100

    Adds router with switch logic

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,33 +1,3 @@
-import * as listeners from "./handlers/index.mjs"
-import * as templates from "./templates/index.mjs"
-import * as postMethods from "./api/posts/index.mjs"
+import router from "./router.mjs";
 
-const path = location.pathname;
-
-console.log(path)
-
-if (path === '/profile/login/') {
-    listeners.setLoginFormListener()
-} else if (path === '/profile/register/') {
-    listeners.setRegisterFormListener()
-} else if (path === '/post/create/') {
-    listeners.setCreatePostFormListener()
-} else if (path === '/post/edit/') {
-    listeners.setUpdatePostListener()
-} else if (path === '/profile/edit/') {
-    listeners.setUpdateProfileListener()
-}
-
-//async function testTemplate() {
-  //  const posts = await postMethods.getPosts();
-    //const container = document.querySelector("#posts");
-    //templates.renderPostTemplates(posts, container);
-//}
-
-//testTemplate()
-
-// post.createPost()
-// post.updatePost()
-// post.removePost()
-// post.getPost()
-// postMethods.getPosts().then(console.log)
+router();
---
+++ src/js/router.mjs
@@ -0,0 +1,23 @@
+import * as listeners from "./handlers/index.mjs"
+
+export default function router() {
+    const path = location.pathname;
+
+    switch (path) {
+        case '/profile/login/':
+            listeners.setLoginFormListener()
+            break;
+        case '/profile/register/':
+            listeners.setRegisterFormListener()
+            break;
+        case '/post/create/':
+            listeners.setCreatePostFormListener()
+            break;
+        case '/post/edit/':
+            listeners.setUpdatePostListener()
+            break;
+        case '/profile/edit/':
+            listeners.setUpdateProfileListener()
+            break;
+    }
+}
