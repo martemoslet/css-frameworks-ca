b24672e9387bde21c96eabd38939adb650092001
Fermain <Fermain@users.noreply.github.com>
2022-12-15 13:57:20 +0200

    Teacher feedback

--- src/js/templates/post.mjs
+++ src/js/templates/post.mjs
@@ -1,28 +1,7 @@
 import { postContainer } from "./postContainer.mjs"
 
-export function postTemplateA(postData) {
-    return `<div class="post" id=${postData.id}>${postData.title}</div>`
-}
-
-export function postTemplateB(postData) {
-    const post = document.createElement("div");
-    post.classList.add("post");
-    post.innerText = postData.title;
-    post.innerText = postData.body;
-
-    if (postData.media) {
-        const img = document.createElement('img');
-        img.src = postData.media;
-        img.alt = `Image from ${postData.title}`
-        post.append(img);
-    }
-
-    return post;
-}
-
 export function renderPostTemplate(postData, parent) {
-   parent.innerHTML += postContainer(postData)
-   // parent.append(postTemplateB(postData))
+    parent.innerHTML += postContainer(postData)
 }
 
 export function renderPostTemplates(postDataList, parent) {
--- src/js/templates/postContainer.mjs
+++ src/js/templates/postContainer.mjs
@@ -2,15 +2,17 @@
 
 export function postContainer(postData) {
     return `
-    <div class="col">
-    <a href="/post?id=${postData.id}">
-    <div class="thumbnail">
-        <div class="card">
-            <img class="card-img-top" src="${postData.media}" alt="${postData.title}">
-            <div class="card-body">
-            <h2 class="card-title">${postData.title}</h2>
-            <p class="card-text">${postData.body}</p>
-        </div>
-    </div>
-    </a></div>`;
+    <div class="col-12 col-md-6">
+        <a href="/post/?id=${postData.id}">
+            <div class="thumbnail">
+                <div class="card">
+                    <img class="card-img" src="${postData.media}" alt="${postData.title}" />
+                    <div class="card-body">
+                        <h2 class="card-title">${postData.title}</h2>
+                        <p class="card-text">${postData.body}</p>
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>`;
 }
