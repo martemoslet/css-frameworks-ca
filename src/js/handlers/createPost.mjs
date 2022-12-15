8cff655bcc8dfac1c62a7878b325b5e6f58042ae
martemoslet <martemoslet@gmail.com>
2022-12-15 23:30:37 +0100

    Updates post form

--- src/js/handlers/createPost.mjs
+++ src/js/handlers/createPost.mjs
@@ -4,11 +4,13 @@ export function setCreatePostFormListener() {
   const form = document.querySelector("#createPost");
 
   if (form) {
-    form.addEventListener("submit", (event) => {
+    form.addEventListener("submit", async (event) => {
       event.preventDefault();
       const form = event.target;
       const formData = new FormData(form);
       const post = Object.fromEntries(formData.entries());
+      await createPost(post);
+      location.href = "/posts/";
 
       // Send it to the API
       createPost(post)
