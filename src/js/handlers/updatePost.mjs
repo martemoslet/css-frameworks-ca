97546fa33e8deb39350d6e66f6684349cf2d1346
martemoslet <martemoslet@gmail.com>
2022-12-17 23:23:51 +0100

    Updates redirect

--- src/js/handlers/updatePost.mjs
+++ src/js/handlers/updatePost.mjs
@@ -19,13 +19,15 @@ export async function setUpdatePostListener() {
 
     button.disabled = false;
 
-    form.addEventListener("submit", (event) => {
+    form.addEventListener("submit", async (event) => {
       event.preventDefault();
       const form = event.target;
       const formData = new FormData(form);
       const post = Object.fromEntries(formData.entries());
       post.id = id;
       alert("Post updated");
+      await updatePost(post);
+      location.href = "/posts/";
 
       // Send it to the API
       updatePost(post);
